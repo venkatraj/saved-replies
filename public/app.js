7367f6267c63b6dd487e15271ee50c62400f8650
Venkat <venkat@codinggeek.com>
2020-08-13 12:21:36 +0530

    jsx and expressions

--- public/app.js
+++ public/app.js
@@ -1,7 +1,46 @@
 "use strict";
 
+// const name = 'Vue';
+var subject = {
+  name: 'React',
+  description: 'A JavaScript library for building user interfaces'
+}; // Exercise, to that for Angular or Vue
+// https://randomuser.me/photos
+
+var person = {
+  firstName: 'Venkat',
+  lastName: 'Raj',
+  profilePic: 'https://randomuser.me/api/portraits/men/66.jpg'
+};
+
+var formatName = function formatName(person) {
+  return "".concat(person.firstName, " ").concat(person.lastName);
+}; // const el = (
+//   <div id="content">
+//     <h1>Hello, {subject.name}</h1>
+//     <p>{subject.description}</p>
+//   </div>
+// );
+// functions are expressions.
+// Element attribute values can be an expression
+// const el = (
+//   <div id="person">
+//     <h1>Hello, {formatName(person)}</h1>
+//     <img src={person.profilePic} alt="That is not me!" />
+//   </div>
+// );
+// Even jsx itself is an expression
+
+
+var getGreeting = function getGreeting() {
+  return /*#__PURE__*/React.createElement("h1", null, "Hello, ", formatName(person));
+};
+
 var el = /*#__PURE__*/React.createElement("div", {
-  id: "content"
-}, /*#__PURE__*/React.createElement("h1", null, "Hello, React"), /*#__PURE__*/React.createElement("p", null, "A JavaScript library for building user interfaces"));
+  id: "person"
+}, getGreeting(), /*#__PURE__*/React.createElement("img", {
+  src: person.profilePic,
+  alt: "That is not me!"
+}));
 var root = document.getElementById('root');
 ReactDOM.render(el, root);
--- src/app.js
+++ src/app.js
@@ -1,7 +1,46 @@
+// const name = 'Vue';
+
+const subject = {
+  name: 'React',
+  description: 'A JavaScript library for building user interfaces',
+};
+
+// Exercise, to that for Angular or Vue
+
+// https://randomuser.me/photos
+const person = {
+  firstName: 'Venkat',
+  lastName: 'Raj',
+  profilePic: 'https://randomuser.me/api/portraits/men/66.jpg',
+};
+
+const formatName = (person) => {
+  return `${person.firstName} ${person.lastName}`;
+};
+
+// const el = (
+//   <div id="content">
+//     <h1>Hello, {subject.name}</h1>
+//     <p>{subject.description}</p>
+//   </div>
+// );
+
+// functions are expressions.
+// Element attribute values can be an expression
+// const el = (
+//   <div id="person">
+//     <h1>Hello, {formatName(person)}</h1>
+//     <img src={person.profilePic} alt="That is not me!" />
+//   </div>
+// );
+
+// Even jsx itself is an expression
+const getGreeting = () => <h1>Hello, {formatName(person)}</h1>;
+
 const el = (
-  <div id="content">
-    <h1>Hello, React</h1>
-    <p>A JavaScript library for building user interfaces</p>
+  <div id="person">
+    {getGreeting()}
+    <img src={person.profilePic} alt="That is not me!" />
   </div>
 );
 
