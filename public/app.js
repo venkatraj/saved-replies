9e933d1cd73b93d1d03a35035e8d2253080f1bbb
Venkat <venkat@codinggeek.com>
2020-08-13 11:39:09 +0530

    babel setup

---
+++ public/app.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var el = /*#__PURE__*/React.createElement("div", {
+  id: "content"
+}, /*#__PURE__*/React.createElement("h1", null, "Hello, React"), /*#__PURE__*/React.createElement("p", null, "A JavaScript library for building user interfaces"));
+var root = document.getElementById('root');
+ReactDOM.render(el, root);
---
+++ src/app.js
@@ -0,0 +1,10 @@
+const el = (
+  <div id="content">
+    <h1>Hello, React</h1>
+    <p>A JavaScript library for building user interfaces</p>
+  </div>
+);
+
+const root = document.getElementById('root');
+
+ReactDOM.render(el, root);
